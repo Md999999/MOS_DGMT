f550e51260f1e2d7da27985271ed8b08c2f61262
Md999999 <160726056+Md999999@users.noreply.github.com>
2025-07-28 16:54:23 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -4,12 +4,14 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sos_app.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread" : False})
-SessionLocal = sessionmaker(bind=engine,autoflush=False,autocommit=False)
-Base= declarative_base()
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+Base = declarative_base()
 
 def get_db():
-    db= SessionLocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
